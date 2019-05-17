6d3b458402700bc98fccb18e322486752ed6d007
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2019-05-17 12:58:55 +0300

    Local configs (#12)

--- changelog-endpoint/src/main/kotlin/jp/co/soramitsu/bootstrap/changelog/config/ChangelogAppConfiguration.kt
+++ changelog-endpoint/src/main/kotlin/jp/co/soramitsu/bootstrap/changelog/config/ChangelogAppConfiguration.kt
@@ -1,16 +1,16 @@
 package jp.co.soramitsu.bootstrap.changelog.config
 
-import com.d3.commons.config.loadConfigs
+import com.d3.commons.config.loadRawLocalConfigs
 import jp.co.soramitsu.iroha.java.IrohaAPI
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 val changelogConfig =
-    loadConfigs(
+    loadRawLocalConfigs(
         prefix = "changelog",
         type = ChangelogConfig::class.java,
         filename = "/changelog.properties"
-    ).get()
+    )
 
 @Configuration
 class ChangelogAppConfiguration {
