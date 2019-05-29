b4e376e6465fdb0505e71054d806fb59fb8cbe11
Alexey <Alexey.N.Chernyshov@gmail.com>
2019-05-29 12:20:10 +0300

    Add copyright

--- samples/sample_changelog.groovy
+++ samples/sample_changelog.groovy
@@ -1,3 +1,8 @@
+/*
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import jp.co.soramitsu.bootstrap.changelog.ChangelogAccountPublicInfo
 import jp.co.soramitsu.bootstrap.changelog.ChangelogInterface
 import jp.co.soramitsu.bootstrap.changelog.ChangelogPeer
