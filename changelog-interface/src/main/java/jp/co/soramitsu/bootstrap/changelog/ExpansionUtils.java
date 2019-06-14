cab234e60f9d25b0ee9ff46fc9b7b61e535e3c8b
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2019-06-14 10:30:58 +0300

    Feature/expansion details (#23)

---
+++ changelog-interface/src/main/java/jp/co/soramitsu/bootstrap/changelog/ExpansionUtils.java
@@ -0,0 +1,33 @@
+package jp.co.soramitsu.bootstrap.changelog;
+
+import com.google.gson.Gson;
+import jp.co.soramitsu.iroha.java.Transaction;
+import jp.co.soramitsu.iroha.java.Utils;
+
+public class ExpansionUtils {
+
+    private static final Gson gson = new Gson();
+
+    private ExpansionUtils() {
+
+    }
+
+    /**
+     * Creates transaction that triggers the expansion process
+     *
+     * @param expansionDetails - details of expansion
+     * @param triggerAccountId - account that is used as a trigger
+     * @return unsigned transaction
+     */
+    public static Transaction createExpansionTriggerTx(
+            ExpansionDetails expansionDetails,
+            String triggerAccountId) {
+        return Transaction
+                .builder(ChangelogInterface.superuserAccountId)
+                .setAccountDetail(
+                        triggerAccountId,
+                        String.valueOf(System.currentTimeMillis()),
+                        Utils.irohaEscape(gson.toJson(expansionDetails))
+                ).build();
+    }
+}
